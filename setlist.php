5d54f404f3608a524a17684d920cbd9082e5fcf0
spiffyrob <robgrabill@gmail.com>
2017-10-20 15:01:57 -0400

    Update setlist.php

--- setlist.php
+++ setlist.php
@@ -29,7 +29,7 @@ $decode = json_decode($response);
 $array = json_decode(json_encode($decode), true);
 $exists = ($array["success"]);
 if ($exists==NULL){
-  echo $showdate;
+  echo "Setlist Not Found";
 } else {
 
 $setlist = "----------------\n";
