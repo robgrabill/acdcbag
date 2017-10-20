2f9b9f032255ff82c6315b15556329c4a3b04759
spiffyrob <robgrabill@gmail.com>
2017-10-20 14:47:28 -0400

    Update gateway.php

--- gateway.php
+++ gateway.php
@@ -5,12 +5,15 @@ include('randomfire.php');
 $parse = $_POST['text'];
 if (strpos($parse, 'setlist') !== false) {
     $parameter= substr(strstr("setlist"," "), 1); 
+    echo $parameter;
     setlist_function();
 } elseif (strpos($parse, 'jam') !==false) {
     $parameter= substr(strstr("jam"," "), 1); 
+    echo $parameter;
     jam_function();
 } elseif (strpos($parse, 'randomfire') !==false) {
     $parameter= substr(strstr("randomfire"," "), 1); 
+    echo $parameter;
     randomfire_function();
 } else {
    echo "Foggy, rather groggy. Try again.";
